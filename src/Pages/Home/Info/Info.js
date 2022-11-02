7b918e403bf6fb9d9f4d3d5b0cc19fbab964dc9a
sakib213 <sakibhss438@gmail.com>
2022-11-02 23:15:56 +0600

    update

--- src/Pages/Home/Info/Info.js
+++ src/Pages/Home/Info/Info.js
@@ -7,7 +7,7 @@ import phone from '../../../assets/icons/phone.svg';
 
 const Info = () => {
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 '>
+        <div className='grid sm:grid-cols-1 lg:grid-cols-3 gap-5 '>
             <InfoCart cardTitle="Opening Hours" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCart>
             <InfoCart cardTitle="Ours Locations" bgClass="bg-accent" img={marker}></InfoCart>
             <InfoCart cardTitle="Contact Us" bgClass="bg-gradient-to-r from-secondary to-primary " img={phone}></InfoCart>
--- src/Pages/Home/Services.js
+++ src/Pages/Home/Services.js
@@ -32,7 +32,7 @@ const Services = () => {
            <h3 className='text-primary text-xl font-bold uppercase'>our services</h3>
             <h2 className='font-bold'>Services We Provide</h2>
            </div>
-           <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
             {
 
                 services.map(service => <Service
