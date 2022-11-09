8fb608b6bd072237161951f8ae2d174eb1691b04
sakib213 <sakibhss438@gmail.com>
2022-11-09 23:55:00 +0600

    update for responsive

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -44,7 +44,7 @@ const location = useLocation();
 
   };
   return (
-    <div className='flex  h-screen justify-center items-center'>
+    <div className='flex min-h-screen justify-center items-center'>
       <div className="card w-96 bg-base-100 shadow-xl">
         <div className="card-body">
           <h2 className="text-center text-xl font-bold">Login</h2>
